217bc6001e9f8a30025a59b7d0f7d3d68c455e55
sherwinski <sherwin@imgix.com>
2021-07-09 11:34:10 -0700

    config(postcss): add postcss-banner plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
+const licenseBanner = require("./build/banner");
+
 module.exports = {
   plugins: [
     require("cssnano")({
       preset: "advanced"
+    })
+    ,
+    require("postcss-banner")({
+      banner: licenseBanner,
+      important: true
     })
   ]
 };
